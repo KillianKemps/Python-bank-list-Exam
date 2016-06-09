837c199ef2557b5fbb700cd39d7e7385c5dd687f
Killian Kemps <killian.kemps@etu-webschoolfactory.fr>
2016-06-09 15:13:37 +0200

    Move bank schema

---
+++ bank_api/bank/domain.py
@@ -0,0 +1,23 @@
+bank = {
+    'schema':{
+        'approvalfy': {
+            'type': 'integer'
+        },
+        'lendprojectcost': {
+            'type': 'integer'
+        },
+        'board_approval_month': {
+            'type': 'string'
+        },
+        'countryname': {
+            'type': 'string'
+        },
+        'docty': {
+            'type': 'string'
+        },
+        'impagency': {
+            'type': 'string'
+        },
+    }
+}
+
--- bank_api/settings.py
+++ bank_api/settings.py
@@ -1,30 +1,9 @@
-bank_schema = {
-    'schema':{
-        'approvalfy': {
-            'type': 'integer'
-        },
-        'lendprojectcost': {
-            'type': 'integer'
-        },
-        'board_approval_month': {
-            'type': 'string'
-        },
-        'countryname': {
-            'type': 'string'
-        },
-        'docty': {
-            'type': 'string'
-        },
-        'impagency': {
-            'type': 'string'
-        },
-    }
-}
+from .bank.domain import bank
 
 DEFAULT_SETTINGS = {
     'MONGO_DBNAME' : 'api-python-exam',
     'RESOURCE_METHODS' : ['GET'],
     'DOMAIN': {
-        'bank': bank_schema
+        'bank': bank
     }
 }
