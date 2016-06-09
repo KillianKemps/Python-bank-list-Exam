f9cb7cb15b7eca2256c394c5508f7b2e4da90865
Killian Kemps <killian.kemps@etu-webschoolfactory.fr>
2016-06-09 14:55:48 +0200

    Expose 6 fields from data

--- settings.py
+++ settings.py
@@ -1,1 +1,28 @@
-DOMAIN = {'people': {}}
+MONGO_DBNAME = 'api-python-exam'
+
+RESOURCE_METHODS = ['GET']
+
+bank_schema = {
+    'schema':{
+        'approvalfy': {
+            'type': 'integer'
+        },
+        'lendprojectcost': {
+            'type': 'integer'
+        },
+        'board_approval_month': {
+            'type': 'string'
+        },
+        'countryname': {
+            'type': 'string'
+        },
+        'docty': {
+            'type': 'string'
+        },
+        'impagency': {
+            'type': 'string'
+        },
+    }
+}
+
+DOMAIN = {'bank': bank_schema}
